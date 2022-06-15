62414b4a5303d55391b0bf80d13cf9cae0234e6e
Brett Bailey <bretto36@hotmail.com>
2022-06-16 06:51:12 +1000

    Removed Zofe Burp

--- src/routes.php
+++ src/routes.php
@@ -1,18 +1,18 @@
 <?php
 
 //dataform routing
-Burp::post(null, 'process=1', array('as'=>'save', function() {
+/*Burp::post(null, 'process=1', array('as'=>'save', function() {
     BurpEvent::queue('dataform.save');
-}));
+}));*/
 
 //datagrid routing
-Burp::get(null, 'page/(\d+)', array('as'=>'page', function($page) {
+/*Burp::get(null, 'page/(\d+)', array('as'=>'page', function($page) {
     BurpEvent::queue('dataset.page', array($page));
 }));
 Burp::get(null, 'ord=(-?)(\w+)', array('as'=>'orderby', function($direction, $field) {
     $direction = ($direction == '-') ? "DESC" : "ASC";
     BurpEvent::queue('dataset.sort', array($direction, $field));
-}))->remove('page');
+}))->remove('page');*/
 
 //todo: dataedit  
 
